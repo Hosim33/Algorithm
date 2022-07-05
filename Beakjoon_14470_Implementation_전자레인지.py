9f79705f80f64d9cd8f04ee51b4db8941168de4b
Hosim33 <63397914+Hosim33@users.noreply.github.com>
2022-07-06 04:09:16 +0900

    Add files via upload

---
+++ Beakjoon_14470_Implementation_전자레인지.py
@@ -0,0 +1,9 @@
+temp1=int(input())
+temp2=int(input())
+time1=int(input())
+time2=int(input())
+time3=int(input())
+if temp1<0:
+    print(abs(temp1)*time1+time2+temp2*time3)
+else:
+    print((temp2-temp1)*time3)
